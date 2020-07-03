10e6f0536f2f5e4a812246ab666ab8c1e303c9e6
Tay Kang Sheng <kangsheng@gmail.com>
2020-07-03 15:12:16 +0800

    default telegram bot to disable link previews

--- eagerlib/telegram_bot_api.rb
+++ eagerlib/telegram_bot_api.rb
@@ -3,8 +3,8 @@ class TelegramBotApi
     @base_url = "https://api.telegram.org/bot#{token}"
   end
 
-  def send_message(chat_id, text, parse_mode = "Markdown")
-    query_params = "chat_id=#{chat_id}&text=#{text}&parse_mode=#{parse_mode}"
+  def send_message(chat_id, text, parse_mode = "Markdown", disable_web_page_preview = "True")
+    query_params = "chat_id=#{chat_id}&text=#{text}&parse_mode=#{parse_mode}&disable_web_page_preview=#{disable_web_page_preview}"
     parsed_url = Addressable::URI.parse("#{@base_url}/sendMessage?#{query_params}")
     response = HTTParty.get(parsed_url.normalize.to_s)
 
