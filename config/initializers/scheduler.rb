0da0239cbead0b6bc5f234ba38c243b9469b8f96
Tay Kang Sheng <kangsheng@gmail.com>
2023-08-26 12:00:08 +0800

    only allow webserver to run scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,7 +2,7 @@ require 'rufus-scheduler'
 
 s = Rufus::Scheduler.singleton
 
-return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake'
+return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake' || ENV["INCLUDES_SCHEDULER"].present?
 
 # Send weekly news updates on Friday 5:30pm
 s.cron '30 17 * * 5' do
