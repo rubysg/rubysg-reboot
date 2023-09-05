69f129302b1433f368be64d23eea5a0afe6a7bf3
Tay Kang Sheng <kangsheng@gmail.com>
2023-09-05 23:06:35 +0800

    add retry and failure notification to organiser telegram chat

--- app/jobs/send_weekly_updates_job.rb
+++ app/jobs/send_weekly_updates_job.rb
@@ -2,12 +2,13 @@ class SendWeeklyUpdatesJob < ApplicationJob
   queue_as :default
 
   retry_on(StandardError, attempts: 3) do |job, error|
-    Rails.logger.debug e.full_message
+    Rails.logger.debug error.full_message
+    job.send_failure_notification(error)
   end
 
   def perform(id)
-    sub = RubySgBotSubscriber.find_by(chat_id: id.to_s)
-    featured_articles = ruby_weekly.featured_articles
+    sub = RubySgBotSubscriber.find_by!(chat_id: id.to_s)
+    featured_articles = RubyWeekly.new.featured_articles
 
     send_telegram_message(
       sub.chat_id,
@@ -54,7 +55,20 @@ class SendWeeklyUpdatesJob < ApplicationJob
     )
   end
 
-  private
+  def send_failure_notification(error)
+    return unless ENV["BOB_OPS_RUBYSG_ORGANISER_INCOMING_WEBHOOK_KEY"].presence
+
+    HTTParty.post(
+      'https://bobops.net/api/incoming_webhook',
+      body: {
+        text: <<~NOTIFICATION
+          #{error.class.name}@SendWeeklyUpdatesJob
+          #{error.message}
+        NOTIFICATION
+      }.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
 
   def send_telegram_message(chat_id, message)
     Telegram.bot.send_message(
@@ -63,9 +77,5 @@ class SendWeeklyUpdatesJob < ApplicationJob
       parse_mode: :Markdown,
       disable_web_page_preview: true
     )
-  end
-
-  def ruby_weekly
-    RubyWeekly.new
   end
 end
--- spec/jobs/send_weekly_updates_job_spec.rb
+++ spec/jobs/send_weekly_updates_job_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SendWeeklyUpdatesJob, type: :job do
+  include ActiveJob::TestHelper
+
   it "enqueues a job" do
     expect { SendWeeklyUpdatesJob.perform_later("1") }.to have_enqueued_job
+  end
+
+  it "retry_on StandardError" do
+    expect_any_instance_of(SendWeeklyUpdatesJob).to receive(:perform).and_raise(StandardError)
+
+    perform_enqueued_jobs do
+      expect_any_instance_of(SendWeeklyUpdatesJob)
+        .to receive(:retry_job)
+
+      SendWeeklyUpdatesJob.perform_later("1")
+    end
+  end
+
+  it "sends failure notification when all retries are exhausted" do
+    stub_request(:post, "https://bobops.net/api/incoming_webhook")
+      .with(
+        body: {
+          "text": <<~ERROR_MESSAGE
+            ActiveRecord::RecordNotFound@SendWeeklyUpdatesJob
+            Couldn't find RubySgBotSubscriber
+          ERROR_MESSAGE
+        }.to_json,
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'User-Agent'=>'Ruby'
+        }
+      )
+      .to_return(status: 200, body: "", headers: {})
+
+    assert_performed_jobs 3 do
+      SendWeeklyUpdatesJob.perform_later("1")
+    end
   end
 end
